22ce6c02526f7f4264475f5ee468a33679a3eed9
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-11-30 21:00:06 -0800

    add button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
+import clsx from "clsx"
 import Link from "next/link"
 
-const baseStyle = {
+const baseStyles = {
     solid: 
         'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2',
     outline:
@@ -39,3 +40,24 @@ type ButtonProps = (
         href?: undefined
     })
 )
+
+export function Button({className, ...props}: ButtonProps) {
+    props.variant ??= 'solid'
+    props.color ??= 'slate'
+
+    className = clsx(
+         baseStyles[props.variant],
+         props.variant === 'outline'
+            ? variantStyles.outline[props.color]
+            : props.variant === "solid"
+            ? variantStyles.solid[props.color]
+            :undefined,
+        className
+    )
+
+    return typeof props.href === 'undefined' ? (
+        <button className={className} {...props} />
+    ) : (
+        <Link className={className} {...props} />
+    )
+}
