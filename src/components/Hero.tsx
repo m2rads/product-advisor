e41434f6e6f6a1bbe9c89ddddfaa3057ae7fd4eb
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-11-30 21:45:30 -0800

    add icons and buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,20 @@ import { Button } from "./Button"
 
 export function Hero () {
     return (
-       <Container className="pb-40 pt-60 text-center">
+       <Container className="pb-40 pt-60 mt-20 sm:mt-0 text-center">
                 <h1 className="mx-auto max-w-4xl text-5xl font-medium tracking-tight sm:text-7xl">Product Advisor</h1>
                 <p className="mx-auto mt-10 text-xl max-w-2xl tracking-tight text-gray-400">Select a method to look for a product</p>
-                <div className="mt-10 flex justify-center gap-x-6">
+                <div className="mt-20 flex justify-center gap-x-6">
                     <Button>
-                        icon
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                        </svg>
+                    </Button>
+                    <Button>
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                        </svg>
                     </Button>
                 </div>
        </Container>
