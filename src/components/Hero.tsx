81d5966be5bcde81ad02f18aef136cd3018ddfd5
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-02 00:26:28 -0800

    update href

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero () {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
                         </svg>
                     </Button>
-                    <Button>
+                    <Button href="/search/product-name">
                         <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
                         </svg>
