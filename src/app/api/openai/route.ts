30b0763de1a2ecf361d00796676941993530b634
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-05 01:15:50 -0800

    add placeholder for openai api handler

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -1,13 +1,15 @@
-import OpenAI from "openai"
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+// import OpenAI from "openai"
+// import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { NextRequest, NextResponse } from "next/server"
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-})
+// const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_API_KEY,
+// })
 
 export const runtime = 'edge'
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
+    const productName = await req.json();
     // const { messages } = await req.json()
     // const response = await openai.chat.completions.create({
     //      model: 'gpt-3.5-turbo',
@@ -19,5 +21,12 @@ export async function POST(req: Request) {
     
     // return new StreamingTextResponse(stream)
 
-    return new Response("well hello")
+    return new NextResponse(JSON.stringify({
+        productName,
+    }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+          },
+    })
 }
