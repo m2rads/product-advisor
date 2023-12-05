b7841948e466f9cce63ca99b2fbb06a01d310563
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-05 01:15:01 -0800

    add data fetching from the endpoint:

--- src/app/search/showcase/page.tsx
+++ src/app/search/showcase/page.tsx
@@ -1,18 +1,20 @@
 'use client'
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Showcase() {
+    const [data, setData] = useState(null);
     const searchParams = useSearchParams()
-
     const productName = searchParams.get("productname")
-    console.log(productName)
+
+    useEffect(() => {
+
+    }, [productName])
 
     return(
         <div>
             <p>showcase</p>
             {productName}
         </div>
-        
-    
     )
 }
