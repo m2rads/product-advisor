87b64afcfb3381404625f02dfac4a2941c25c44c
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-05 01:28:34 -0800

    add ability to fetch data

--- src/app/search/showcase/page.tsx
+++ src/app/search/showcase/page.tsx
@@ -8,13 +8,35 @@ export default function Showcase() {
     const productName = searchParams.get("productname")
 
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/api/openai`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({ productName }),
+                })
+                if (!response.ok) {
+                  throw new Error('Network response was not ok')
+                }
+                const data = await response.json();
+                console.log("data: ", data)
+                setData(data);
+              } catch (error) {
+                  console.log('Error fetching Github stats', error);
+              } finally {
+                // this is for setLoading state 
+              }
+        }
+        fetchData()
 
-    }, [productName])
+    }, [productName]);
 
     return(
         <div>
             <p>showcase</p>
-            {productName}
+            {JSON.stringify(data, null, 2)}
         </div>
     )
 }
