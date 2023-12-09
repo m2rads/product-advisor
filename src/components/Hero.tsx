5d4e1c4df501736e0c76069c1487060c2ea07d50
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-09 11:57:37 -0800

    add conditional rendering

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ const BackButton : React.FC<BackButtonProps> = ({setShowScan, setShowInput} ) =>
                 <Container>
                     <nav className="relative z-50 flex justify-between">
                     <div className="flex items-center md:gap-x-12">
-                    <button onClick={(e) => handleBackClick()} type="button" className="w-full flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                    <button onClick={(e) => handleBackClick()} type="button" className=" flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
                         <svg className="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                         </svg>
@@ -42,7 +42,7 @@ export function Hero () {
 
 
     return (
-       <div>
+       <>
             {showScan && (
                 <>
                     <BackButton
@@ -87,6 +87,6 @@ export function Hero () {
                 </Container>
             )}
             
-       </div>
+       </>
     )
 }
