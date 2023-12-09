352f7ac9e7d226438ae7b19a2ca5a7e9bdda37d7
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-09 15:55:13 -0800

    add loading ui

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -1,28 +1,36 @@
-// import OpenAI from "openai"
-// import { OpenAIStream, StreamingTextResponse } from 'ai'
+import OpenAI from "openai"
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs"
+import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { NextRequest, NextResponse } from "next/server"
 
-// const openai = new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY,
-// })
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
 
 export const runtime = 'edge'
 
 export async function POST(req: NextRequest) {
     const { productName } = await req.json();
-    // const { messages } = await req.json()
-    // const response = await openai.chat.completions.create({
-    //      model: 'gpt-3.5-turbo',
-    //      stream: true,
-    //      messages,
-    // })
+    // sanitize the product name: 
+    const productMessage: ChatCompletionMessageParam = {
+        role: 'user', 
+        content: `This is a product sku: ${productName}. We would like you to give us the features and specs of this product in bullet points. Avoid returning extra phrases and sentences. We are only interested in bullet lists. If you couldn't find any information about the product, either return the message 'no product information found. please try again.' or send some suggestions like: 'we couldn't find this product. Did you mean "this product" (suggest the closest option to it)?'`
+    };
+    
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        stream: false,
+        messages: [productMessage],
+    });
 
     // const stream = OpenAIStream(response)
     
     // return new StreamingTextResponse(stream)
 
+    console.log(response)
+
     return new NextResponse(JSON.stringify({
-        productName,
+        response,
     }), {
         status: 200,
         headers: {
---
+++ src/app/showcase/loading.tsx
@@ -0,0 +1,9 @@
+// 'use client'
+export default function Loading() {
+    return (
+        <div className="text-center">
+            <h1>Loading...</h1>
+            <p>hopefully for not too long :)</p> 
+        </div>
+    )
+}
