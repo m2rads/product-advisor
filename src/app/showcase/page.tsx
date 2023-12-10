c54733b788cd02b4c248594c460815662cefc99b
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-09 23:53:47 -0800

    enhanced product showcase design

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -2,47 +2,49 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Loading from "./loading";
+import { ProductCard } from "@/components/ProductCard";
 
 export default function Page() {
     const [data, setData] = useState(null);
     const searchParams = useSearchParams()
     const productName = searchParams.get("productname")
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`/api/openai`, {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({ productName }),
-                })
-                if (!response.ok) {
-                  throw new Error('Network response was not ok')
-                }
-                const data = await response.json();
-                console.log("data: ", data)
-                setData(data);
-              } catch (error) {
-                  console.log('Error fetching Github stats', error);
-              } finally {
-                // this is for setLoading state 
-              }
-        }
-        fetchData()
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(`/api/openai`, {
+    //               method: 'POST',
+    //               headers: {
+    //                 'Content-Type': 'application/json',
+    //               },
+    //               body: JSON.stringify({ productName }),
+    //             })
+    //             if (!response.ok) {
+    //               throw new Error('Network response was not ok')
+    //             }
+    //             const data = await response.json();
+    //             console.log("data: ", data)
+    //             setData(data);
+    //           } catch (error) {
+    //               console.log('Error fetching Github stats', error);
+    //           } finally {
+    //             // this is for setLoading state 
+    //           }
+    //     }
+    //     fetchData()
 
-    }, [productName]);
+    // }, [productName]);
 
     return(
         <div>
-            {data && (
+            {!data && (
                 <>
-                    <p>showcase</p>
-                    {JSON.stringify(data, null, 2)}
+                    {/* <p>showcase</p>
+                    {JSON.stringify(data, null, 2)} */}
+                    <ProductCard />
                 </>
             )}
-            {!data && (
+            {data && (
                 <>
                     <Loading />
                 </>
