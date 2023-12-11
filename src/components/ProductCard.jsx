28a667eb75c67ce4bdd76c2e6736db594507d960
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-10 19:43:06 -0800

    display streaming texts

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import sonyHeadphone from '../images/sony.jpeg'
+import Image from 'next/image';
+import { Tab } from '@headlessui/react'
+import { ChatBubbleBottomCenterTextIcon, ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
+import { ChatBox } from './ChatBox';
+
+// interface ProductData {
+//   // id: number;
+//   // name: string;
+//   description: string;
+// }
+
+// interface ProductCardProps {
+//   product: ProductData;
+// }
+
+// need to add props
+export const ProductCard = ({data}) => {
+  const [firstIconActive, setFirstIconActive] = useState(true);
+  const [secondIconActive, setSecondIconActive] = useState(false);
+
+  const bulletPoints = data.split('\n');
+  // console.log(bulletPoints)
+
+  const setActive = (firstIcon, secondIcon) => {
+    setFirstIconActive(firstIcon);
+    setSecondIconActive(secondIcon);
+  }
+
+  return (
+    <div className="flex flex-col w-80 mx-auto mt-5 sm:mt-20">
+      <div className="rounded-xl">
+          <Image 
+              className="rounded-xl mx-auto mb-8 sm:w-2/3 py-4 border px-3"
+              src={sonyHeadphone}
+              alt='product image'
+              width={120}
+              height={100}
+              />
+      </div>
+      <div className="bg-emerald-700 px-4 rounded-xl items-baseline shadow-md p-6">
+        <Tab.Group>
+        <Tab.List className="mb-4">
+          <Tab>
+              <ChatBubbleBottomCenterTextIcon 
+                  onClick={() => setActive(true, false) }
+                  className={`h-6 w-6 m-1 text-gray-900 outline-none ${firstIconActive ? 'opacity-100' : 'opacity-50'}`} 
+              />
+          </Tab>
+          <Tab>
+              <ChatBubbleLeftRightIcon 
+                  onClick={() => setActive(false, true)} 
+                  className={`h-6 w-6 m-1 text-gray-900 outline-none ${secondIconActive ? 'opacity-100' : 'opacity-50'}`} 
+              />
+          </Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            <div className="items-center w-full sm:w-auto justify-end">
+                <h1 className='text-2xl font-bold tracking-tight'>Product Name</h1>
+                <h2 className='text-xl pt-2'>Specs:</h2>
+                {bulletPoints.map((element, index) => (
+                  <div className='p-2' key={index}>{element}</div>
+                ))}
+
+            </div>
+          </Tab.Panel>
+          <Tab.Panel> <ChatBox /> </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
+      </div>
+    </div>
+  );
+};
+
