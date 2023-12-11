c78e78896ad79fa25185065165c9874061708bf5
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-10 19:43:23 -0800

    implement streaming

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -14,27 +14,29 @@ export async function POST(req: NextRequest) {
     // sanitize the product name: 
     const productMessage: ChatCompletionMessageParam = {
         role: 'user', 
-        content: `This is a product sku: ${productName}. We would like you to give us the features and specs of this product in bullet points. Avoid returning extra phrases and sentences. We are only interested in bullet lists. If you couldn't find any information about the product, either return the message 'no product information found. please try again.' or send some suggestions like: 'we couldn't find this product. Did you mean "this product" (suggest the closest option to it)?'`
+        content: `This is a product name: ${productName}. We would like you to give us the features and specs of this product in only 5 bullet points. Keep the specs as short as 1 sentence. Avoid returning extra phrases and sentences. We are only interested in bullet lists. If you couldn't find any information about the product, either return the message 'no product information found. please try again.' or send some suggestions like: 'we couldn't find this product. Did you mean "this product" (suggest the closest option to it)?'. Couple notes about formating each bullet point: Make sure that you send us numbered lists that starts with a number and ends with a scape character like new line character. For example, each bullet list is represented like "1. specification.\n" .`
     };
     
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        stream: false,
+        stream: true,
         messages: [productMessage],
     });
 
-    // const stream = OpenAIStream(response)
+    const stream = OpenAIStream(response)
+    console.log("stream: ", stream)
     
-    // return new StreamingTextResponse(stream)
+    return new StreamingTextResponse(stream)
 
-    console.log(response.choices[0].message.content)
-
-    return new NextResponse(JSON.stringify({
-        response,
-    }), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-          },
-    })
+    // console.log(response.choices[0].message.content)
+    // const content = response.choices[0].message.content
+   
+    // return new NextResponse(JSON.stringify({
+    //     content,
+    // }), {
+    //     status: 200,
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    // })
 }
