5f69434f62238816333c491652af8411ce50ef36
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-11 19:09:38 -0800

    add reusability feature  for handling openai calls

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -10,21 +10,39 @@ const openai = new OpenAI({
 export const runtime = 'edge'
 
 export async function POST(req: NextRequest) {
-    const { productName } = await req.json();
-    // sanitize the product name: 
-    const productMessage: ChatCompletionMessageParam = {
-        role: 'user', 
-        content: `This is a product name: ${productName}. We would like you to give us the features and specs of this product in only 5 bullet points. Keep the specs as short as 1 sentence. Avoid returning extra phrases and sentences. We are only interested in bullet lists. If you couldn't find any information about the product, either return the message 'no product information found. please try again.' or send some suggestions like: 'we couldn't find this product. Did you mean "this product" (suggest the closest option to it)?'. Couple notes about formating each bullet point: Make sure that you send us numbered lists that starts with a number and ends with a scape character like new line character. For example, each bullet list is represented like "1. specification.\n" .`
-    };
+    const { productName, chatInput } = await req.json();
+
+    let message: ChatCompletionMessageParam = {role: 'user', content: ''}
+
+    if (productName) {
+        const productMessage: ChatCompletionMessageParam = {
+            role: 'user', 
+            content: `This is a product name: ${productName}. We would like you to give us the features and specs of this product in only 5 bullet points. Keep the specs as short as 1 sentence. Avoid returning extra phrases and sentences. We are only interested in bullet lists. If you couldn't find any information about the product, either return the message 'no product information found. please try again.' or send some suggestions like: 'we couldn't find this product. Did you mean "this product" (suggest the closest option to it)?'. Couple notes about formating each bullet point: Make sure that you send us numbered lists that starts with a number and ends with a scape character like new line character. For example, each bullet list is represented like "1. specification.\n" .`
+        };
+        message = productMessage
+    } else if (chatInput) {
+        const chatMessage: ChatCompletionMessageParam = {
+            role: 'assistant', 
+            content: `This is my question regarding the product information I recieved from you. Question: ${chatInput}.`
+        };
+
+        message = chatMessage
+    }
     
-    const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        stream: true,
-        messages: [productMessage],
-    });
+    const response = await openaiHandler(message)
 
     const stream = OpenAIStream(response)
     console.log("stream: ", stream)
     
     return new StreamingTextResponse(stream)
+}
+
+async function openaiHandler(message: ChatCompletionMessageParam) { 
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        stream: true,
+        messages: [message],
+    });
+
+    return response;
 }
