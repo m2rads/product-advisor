fa6f0bbb8368310b41e1773fd3d4ad87c2bb65cd
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-11 18:36:01 -0800

    quick fix

--- src/components/PopoverWindow.jsx
+++ src/components/PopoverWindow.jsx
@@ -30,9 +30,9 @@ export function PopoverWindow() {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute bottom-10 left-1/2 z-10 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+              <Popover.Panel className="absolute bottom-10 z-10 w-screen max-w-sm transform px-4 sm:px-0 lg:max-w-3xl">
                 <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
-                  <div className="relative grid gap-8 bg-emerald-700 p-7 lg:grid-cols-2">
+                  <div className="relative grid gap-8 bg-gray-300 p-7 lg:grid-cols-2">
                     <ChatBox />
                   </div>
                 </div>
